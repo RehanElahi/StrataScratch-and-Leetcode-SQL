4e9566fc87a4af62d65a817849085c300b8daf3a
Rehan Elahi <36388494+RehanElahi@users.noreply.github.com>
2023-01-09 03:25:24 +0500

    Find all posts which were reacted to with a heart (Subquery)

---
+++ QuestionID10087(StrataScratch).sql
@@ -0,0 +1,23 @@
+-- Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+
+-- facebook_reactions
+-- poster: int
+-- friend: int
+-- reaction: varchar
+-- date_day: int
+-- post_id: int
+
+-- facebook_posts
+-- post_id: int
+-- poster: int
+-- post_text: varchar
+-- post_keywords: varchar
+-- post_date: datetime
+
+-- Code
+select * from facebook_posts
+where post_id In (
+Select post_id 
+from facebook_reactions
+where reaction= 'heart'
+);
