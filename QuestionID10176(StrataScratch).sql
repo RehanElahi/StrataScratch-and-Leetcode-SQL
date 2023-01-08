544cd82f921854348c0e25fb0e9520670f2d1bb6
Rehan Elahi <36388494+RehanElahi@users.noreply.github.com>
2023-01-09 02:26:08 +0500

    Find the last time each bike was in use

--- QuestionID10176(StrataScratch).sql
+++ QuestionID10176(StrataScratch).sql
@@ -1,22 +1,22 @@
-#Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). 
-#Order the results by bikes that were most recently used.
+-- Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). 
+-- Order the results by bikes that were most recently used.
 
-#Table
-#dc_bikeshare_q1_2012
+-- Table
+-- dc_bikeshare_q1_2012
 
-#duration: varchar
-#duration_seconds: int
-#start_time: datetime
-#start_station: varchar
-#start_terminal: int
-#end_time: datetime
-#end_station: varchar
-#end_terminal: int
-#bike_number: varchar
-#rider_type: varchar
-#id: int
+-- duration: varchar
+-- duration_seconds: int
+-- start_time: datetime
+-- start_station: varchar
+-- start_terminal: int
+-- end_time: datetime
+-- end_station: varchar
+-- end_terminal: int
+-- bike_number: varchar
+-- rider_type: varchar
+-- id: int
 
-#Code
+-- Code
 Select bike_number, Max(end_time) as 'last_used'
 from dc_bikeshare_q1_2012
 Group by bike_number
