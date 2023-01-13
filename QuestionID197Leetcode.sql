0e75b1b81abee03314669539d6610c7205d4fdeb
Rehan Elahi <36388494+RehanElahi@users.noreply.github.com>
2023-01-13 20:30:22 +0500

    Rising Temperature (CTEs, Window functions)

---
+++ QuestionID197Leetcode.sql
@@ -0,0 +1,55 @@
+-- Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+-- Explanation: 
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+-- Code
+with t as (Select *, Lag(temperature) OVER (Order BY recordDate) as 'PrevT', Lag (recordDate) OVER () as r
+From Weather),
+
+f as 
+(
+    Select *, DateDiff(recordDate, r) as k
+    from t
+)
+Select id from f
+where k=1 AND temperature>PrevT
